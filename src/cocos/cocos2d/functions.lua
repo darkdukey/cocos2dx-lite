4e8314754a5f0056559ac48b04666fb5c46e6947
yinjimmy <jimmy.yin5@gmail.com>
2017-10-24 09:32:16 +0800

    table clone

--- src/cocos/cocos2d/functions.lua
+++ src/cocos/cocos2d/functions.lua
@@ -673,6 +673,39 @@ function table.unique(t, bArray)
     return n
 end
 
+function table.clone( org )
+    return {unpack(org)}
+end
+
+function table.shallowcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in pairs(orig) do
+            copy[orig_key] = orig_value
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+function table.deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 string._htmlspecialchars_set = {}
 string._htmlspecialchars_set["&"] = "&amp;"
 string._htmlspecialchars_set["\""] = "&quot;"
